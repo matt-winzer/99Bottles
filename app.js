a1b20b6e31d57a9b735ca569c248d12b22b658b2
Matthew Winzer <matthewwinzer@Matthews-MacBook-Air.local>
2016-11-14 15:18:42 -0700

    append basic horizontal card when option is selected: no content or styling

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 $(document).ready(function() {
+  //Initialize Select
   $('select').material_select();
 
+  //Store URL for styles GET request
   var styleURL = 'http://galvanize-cors-proxy.herokuapp.com/http://api.brewerydb.com/v2/styles/?key=53f372495b64d9d4e9a86e2a8ca999b4';
 
+  //GET request for beer styles
   $.get(styleURL, appendStyles);
+
+  //Event listener for Select change
+
 
 });
 
 function appendStyles(styleData) {
-  console.log(styleData.data.length);
-  console.log(styleData.data[0].name);
-
+  //Iterate through styleData object from GET request
   for (var i = 0; i < styleData.data.length; i++) {
-    $('#beerType').append('<option value="' + (1 + i) + '">' + styleData.data[i].name + '</option>');
+    $('#beerType').append('<option value="' + styleData.data[i].name + '">' + styleData.data[i].name + '</option>');
   }
+  //Re-initialize Select
   $('select').material_select();
+  //On select change, append card with style info
+  $('#beerType').change(addStyleCard);
+
+
+  function addStyleCard() {
+    console.log($(this).val());
+
+    $('#styleCard').append(
+      '<div class="col s12 m7"><h2 class="header">Horizontal Card</h2><div class="card horizontal"><div class="card-image"><img src="http://lorempixel.com/100/190/nature/6"></div><div class="card-stacked"><div class="card-content"><p>I am a very simple card. I am good at containing small bits of information.</p></div><div class="card-action"><a href="#">This is a link</a></div></div></div></div>');
+
+  }
 }
