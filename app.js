3032dce215ca2e332a0511068a2ebe0881eabfee
Matthew Winzer <matthewwinzer@Matthews-MacBook-Air.local>
2016-11-14 14:09:20 -0700

    added GET request to app.js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
   $('select').material_select();
 
+  var styleURL = 'http://galvanize-cors-proxy.herokuapp.com/http://api.brewerydb.com/v2/styles/?key=53f372495b64d9d4e9a86e2a8ca999b4';
+
+  $.get(styleURL, appendStyles);
+
 });
+
+function appendStyles(styleData) {
+  console.log(styleData);
+  console.log(styleData.data[0].name);
+  
+}
